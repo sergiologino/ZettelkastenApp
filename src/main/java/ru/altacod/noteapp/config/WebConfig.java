6550d4fe69ff49e5231e5b0162c2142e90e309ea
savki <savkin@altacod.ru>
2025-06-25 22:55:49 +0300

    Implement resource handler for file uploads in WebConfig class

--- src/main/java/ru/altacod/noteapp/config/WebConfig.java
+++ src/main/java/ru/altacod/noteapp/config/WebConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
 //    @Bean
 //    public WebMvcConfigurer corsConfigurer() {
@@ -35,4 +35,10 @@ public class WebConfig {
 //        };
 //
 //    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:uploads/");
+    }
 }
